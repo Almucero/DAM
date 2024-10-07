101d15eaafac89283a2df84d8995f52a233f12a7
Almucero <alvarokilor@gmail.com>
2024-10-07 11:53:40 +0200

    comision

---
+++ PROGRAMACION/CAP02/Ejercicio7/Ejercicio7/src/Animales/perro.java
@@ -0,0 +1,12 @@
+package Animales;
+
+public class perro {
+    public String nombre;
+    private String dueno;
+    public int edad;
+}
+public perro(String nombre, String dueno, int edad){
+    this.nombre = nombre;
+    this.dueno = dueno;
+    this.edad = edad;
+}
