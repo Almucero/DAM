b7d515ab5294d1deffc5014ad0b030726455fb7b
Almucero <alvarokilor@gmail.com>
2024-10-09 11:15:13 +0200

    commit

--- PROGRAMACION/CAP02/Ejercicio7/Ejercicio7/src/App.java
+++
@@ -1,8 +0,0 @@
-import Animales.perro;
-
-public class App {
-    public static void main(String[] args) throws Exception{
-        Persona persona = new Persona(nombre:"Álvaro", apellidos:"")
-        System.out.print(perro.leerchip());
-    }
-}
