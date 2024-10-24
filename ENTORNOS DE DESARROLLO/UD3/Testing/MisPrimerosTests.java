0c6f297c8957a950f8ebca9b8175c88ba8f046ea
Almucero <alvarokilor@gmail.com>
2024-10-24 14:45:31 +0200

    dfgh

---
+++ ENTORNOS DE DESARROLLO/UD3/Testing/MisPrimerosTests.java
@@ -0,0 +1,6 @@
+public class MisPrimerosTests {
+    public int suma(int sumando1, int sumando2) {
+        int resultado = sumando1 + sumando2;
+        return resultado;
+    }
+}
---
+++ ENTORNOS DE DESARROLLO/UD3/Testing/MisPrimerosTestsTests.java
@@ -0,0 +1,8 @@
+import org.junit.Test;
+
+public class MisPrimerosTestsTests {
+    @Test
+    public void testSuma() {
+
+    }
+}
