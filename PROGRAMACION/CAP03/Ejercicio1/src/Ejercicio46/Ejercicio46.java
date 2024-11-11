41351a2259d7a1fb2bf293572410be1e101521b2
Almucero <alvarokilor@gmail.com>
2024-11-11 23:37:59 +0100

    dfh

--- PROGRAMACION/CAP03/Ejercicio1/src/Ejercicio46/Ejercicio46.java
+++ PROGRAMACION/CAP03/Ejercicio1/src/Ejercicio46/Ejercicio46.java
@@ -1,5 +1,69 @@
 package Ejercicio46;
 
 public class Ejercicio46 {
-    
+    public static void main(String[] args) {
+        boolean cerrar=false;
+        boolean anchuraValida=false;
+        boolean alturaValida=false;
+        int anchura=0;
+        int altura=0;
+        while (cerrar!=true) {
+            try {
+                while (anchuraValida!=true) {
+                    try {
+                        System.out.print("Por favor, introduzca la anchura del rectángulo (como mínimo 2): ");
+                        anchura = Integer.parseInt(System.console().readLine());
+                        if (anchura>=2) {
+                            anchuraValida=true;
+                        }
+                        else {
+                            System.out.println("Lo siento, los datos introducidos no son correctos, el valor mínimo para la anchura es 2");
+                            System.out.println();
+                        }
+                    }
+                    catch (NumberFormatException e) {
+                        System.out.println("Error. Debe introducir un valor válido para la anchura");
+                        System.out.println();
+                    }
+                }
+                while (alturaValida!=true) {
+                    try {
+                        System.out.print("Ahora introduzca la altura (como mínimo 2): ");
+                        altura = Integer.parseInt(System.console().readLine());
+                        if (altura>=2) {
+                            alturaValida=true;
+                        }
+                        else {
+                            System.out.println("Lo siento, los datos introducidos no son correctos, el valor mínimo para la altura es 2");
+                            System.out.println();
+                        }
+                    }
+                    catch (NumberFormatException e) {
+                        System.out.println("Error. Debe introducir un valor válido para la altura");
+                        System.out.println();
+                    }
+                }
+                for (int fila=1; fila<=altura; fila++) {
+                    if (fila==1 || fila==altura) {
+                        for (int columna=1; columna<=anchura; columna++) {
+                            System.out.print("* ");
+                        }
+                    }
+                    else {
+                        System.out.print("*");
+                        for (int columna=2; columna<anchura; columna++) {
+                            System.out.print("  ");
+                        }
+                        System.out.print(" *");
+                    }
+                    System.out.println();
+                }
+                cerrar=true;
+            }
+            catch (Exception e) {
+                System.out.println("Ocurrió un error inesperado");
+                cerrar=true;
+            }
+        }
+    }
 }
--- PROGRAMACION/CAP03/Ejercicio1/src/Ejercicio47/Ejercicio47.java
+++ PROGRAMACION/CAP03/Ejercicio1/src/Ejercicio47/Ejercicio47.java
@@ -1,22 +1,44 @@
 package Ejercicio47;
 
 public class Ejercicio47 {
-    public static void main(String[] args) {
-        System.out.print("Por favor, introduzca la altura (número impar mayor o igual a 5): ");
-        int altura = Integer.parseInt(System.console().readLine());
-        if (altura%2==0 || altura<5) {
-            System.out.print("La altura introducida no es correcta.");
-        }
-        else {
-            for (int fila=1; fila<=altura; fila++) {
-                if (fila==1 || fila==altura/2 || fila==altura) {
-                    System.out.print("MMMMMM");
-                    System.out.println();
+    public static void main(String[] args) throws Exception {
+        boolean cerrar=false;
+        boolean alturaValida=false;
+        int altura=0;
+        while (cerrar!=true) {
+            try {
+                while (alturaValida!=true) {
+                    try {
+                        System.out.print("Por favor, introduzca la altura (número impar mayor o igual a 5): ");
+                        altura = Integer.parseInt(System.console().readLine());
+                        if (altura%2==0 || altura<5) {
+                            System.out.println("La altura introducida no es correcta.");
+                            System.out.println();
+                        }
+                        else {
+                            alturaValida=true;
+                        }
+                    }
+                    catch (NumberFormatException e) {
+                        System.out.println("Error. Debe introducir un valor valido");
+                        System.out.println();
+                    }
                 }
-                else {
-                    System.out.print("M    M");
-                    System.out.println();
+                for (int fila=1; fila<=altura; fila++) {
+                    if (fila==1 || fila==(altura/2)+1 || fila==altura) {
+                        System.out.print("MMMMMM");
+                        System.out.println();
+                    }
+                    else {
+                        System.out.print("M    M");
+                        System.out.println();
+                    }
                 }
+                cerrar=true;
+            }
+            catch (Exception e) {
+                System.out.println("Ocurrió un error inesperado");
+                cerrar=true;
             }
         }
     }
