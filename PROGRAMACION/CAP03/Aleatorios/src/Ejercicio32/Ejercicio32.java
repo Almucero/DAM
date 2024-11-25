0241b1a72c0022bcab6a559bf084d3a576f82980
Almucero <alvarokilor@gmail.com>
2024-11-25 10:23:50 +0100

    kjffbhxc<jkñbxvkñbjcafin

--- PROGRAMACION/CAP03/Aleatorios/src/Ejercicio32/Ejercicio32.java
+++ PROGRAMACION/CAP03/Aleatorios/src/Ejercicio32/Ejercicio32.java
@@ -5,27 +5,25 @@ public class Ejercicio32 {
         String espacios="";
         int aleatorio = (int)(Math.random()*3)+1;
         switch (aleatorio) {
-            case 2-> posicion = posicion+1;
-            case 3-> posicion = posicion-1;
+            case 2-> posicion += 1;
+            case 3-> posicion -= 1;
         };
         for (int i=1; i<=posicion; i++) {
-            espacios = espacios+" ";
+            espacios += " ";
         }
         System.out.print(espacios);
     }
     public static void rellenoSendero() {
-        boolean hayObstaculo=false;
         String obstaculo=" ";
         int aleatorio = (int)(Math.random()*2)+1;
         int aleatorio2 = (int)(Math.random()*2)+1;
         int aleatorio3 = (int)(Math.random()*4)+1;
         switch (aleatorio) {
-            case 1-> hayObstaculo=true;
-        }
-        if (hayObstaculo) {
-            switch (aleatorio2) {
-                case 1-> obstaculo="*";
-                case 2-> obstaculo="0";
+            case 1-> {
+                switch (aleatorio2) {
+                    case 1-> obstaculo="*";
+                    case 2-> obstaculo="0";
+                }
             }
         }
         switch (aleatorio3) {
@@ -35,7 +33,7 @@ public class Ejercicio32 {
             case 4-> System.out.printf("|   %s|%n", obstaculo);
         };
     }
-    public static void main(String[] args) {
+    public static int pedirLongitud() {
         int longitud;
         while (true) {
             try {
@@ -43,24 +41,33 @@ public class Ejercicio32 {
                 longitud = Integer.parseInt(System.console().readLine());
                 if (longitud>=1) {
                     break;
-                } 
+                }
                 else {
                     System.out.println("Error. La longitud del sendero debe ser como mínimo 1\n");
                 }
-            } 
+            }
             catch (NumberFormatException e) {
                 System.out.println("Error. Debe introducir un valor válido: "+e+"\n");
-            } 
+            }
             catch (Exception e) {
                 System.out.println("Ocurrió un error inesperado: "+e+"\n");
             }
         }
-        int posicion=7;
-        System.out.print("       ");
-        rellenoSendero();
-        for (int i=1; i<=longitud-1; i++) {
-            posicionSendero(posicion);
+        return longitud;
+    }
+    public static void main(String[] args) {
+        try {
+            int longitud = pedirLongitud();
+            int posicion=7; //posición = numEspaciosIzquierda, distinto en el primero
+            System.out.print("       "); //7 espacios iniciales
             rellenoSendero();
+            for (int i=1; i<=longitud-1; i++) { //se resta 1 ya que la primera longitud se hace manualmente
+                posicionSendero(posicion);
+                rellenoSendero();
+            }
+        }
+        catch (Exception e) {
+            System.out.println("Ocurrió un error inesperado: "+e);
         }
     }
 }
