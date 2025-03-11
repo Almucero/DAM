8443a5b7c7ed038399361d3a843a53e319448465
Almucero <alvarokilor@gmail.com>
2025-03-11 14:40:18 +0100

    dgh

--- PROGRAMACION/CAP12/BBDDLibros/src/App.java
+++ PROGRAMACION/CAP12/BBDDLibros/src/App.java
@@ -8,27 +8,35 @@ public class App {
 
         Connection conexion = null;
         Statement sentencia = null;
+        Statement sentencia2 = null;
         ResultSet resultado = null;
+        ResultSet resultado2 = null;
 
         try {
             conexion = DriverManager.getConnection(url, usuario, clave);
             String sql = "SELECT ISBN, NumeroCopias, NombreEditorial, TituloLibro FROM librosoriginal";
             String sql2 = "SELECT DNI, Nombre, PrimerApellido, SegundoApellido, Edad, Nacionalidad, Direccion, Genero FROM comprador";
+            /*String sql3 = "SELECT DNI, ISBN, PuntoVenta, Unidades FROM compra";*/
             sentencia = conexion.createStatement();
+            sentencia2 = conexion.createStatement();
 
             resultado = sentencia.executeQuery(sql);
+            resultado2 = sentencia2.executeQuery(sql2);
 
             while(resultado.next()) {
                 String ISBN = resultado.getString("ISBN");
                 int NumeroCopias = resultado.getInt("NumeroCopias");
                 String NombreEditorial = resultado.getString("NombreEditorial");
                 String TituloLibro = resultado.getString("TituloLibro");
-                String DNI = resultado.getString("DNI");
-                String Nombre = resultado.getString("Nombre");
-                String PrimerApellido = resultado.getString("PrimerApellido");
-                String SegundoApellido = resultado.getString("SegundoApellido");
-                Date Edad = 
+                String DNI = resultado2.getString("DNI");
+                String Nombre = resultado2.getString("Nombre");
+                String PrimerApellido = resultado2.getString("PrimerApellido");
+                String SegundoApellido = resultado2.getString("SegundoApellido");
+                Date Edad = resultado2.getDate("Edad");
+                String Nacionalidad = resultado2.getString("Nacionalidad");
+                String Genero = resultado2.getString("Genero");
                 System.out.println("ISBN: "+ISBN+", NumeroCopias: "+NumeroCopias+", NombreEditorial: "+NombreEditorial+", TituloLibro: "+TituloLibro);
+                System.out.println("DNI: "+DNI+", Nombre:"+Nombre+", PrimerApellido: "+PrimerApellido+", SegundoApellido: "+SegundoApellido+", Edad: "+Edad+", Nacionalidad: "+Nacionalidad+", Genero: "+Genero);
             }
         }
         catch(SQLException e) {
@@ -38,7 +46,9 @@ public class App {
             try {
                 if (conexion!=null) conexion.close();
                 if (sentencia!=null) sentencia.close();
+                if (sentencia2!=null) sentencia2.close();
                 if (resultado!=null) resultado.close();
+                if (resultado2!=null) resultado2.close();
             }
             catch (SQLException e) {
                 e.printStackTrace();
